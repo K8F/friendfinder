2161787b67f342f4000fc65fab7fb49fc12c470b
Kate Foust <kate.e.foust@gmail.com>
2020-03-08 18:35:52 -0700

    added test data to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+//friends object and placeholder data
+
+var users=[
+{
+    name: "Henry Chinaski",
+    photo: "https://twitter.com/_Chinaski_Henry/photo",
+    scores:[3, 2, 2, 1, 1, 5, 4, 5, 3, 5]
+},
+
+{
+    name: "Emma",
+    photo: "https://twitter.com/DailyEmmaQuotes/photo",
+    scores: [5, 5, 5, 3, 2, 3, 2, 1, 5, 2]
+}, 
+
+{
+    name: "Daisy Buchanan",
+    photo: "https://twitter.com/DaisyBGatsby/photo",
+    scores: [1, 1, 1, 5, 5, 5, 3, 1, 3, 4]
+},
+
+{
+    name: "Dorian Gray",
+    photo: "https://twitter.com/DorianGrayBFP/photo",
+    scores: [5, 2, 2, 4, 4, 5, 5, 2, 4, 1]
+}
+
+];
+
+module.exports=users;
