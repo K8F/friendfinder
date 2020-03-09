4a330507bf3e7b47664a823979479f570c4a05a3
Kate Foust <kate.e.foust@gmail.com>
2020-03-08 18:14:00 -0700

    added modal to html, wrote script

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,25 @@
+// console.log('API Route Connected');
+
+
+// //dependencies
+// var path =require('path');
+
+// //export API routes
+// module.exports=function(app){
+
+//     //read all friend entries
+
+//     app.get('/api/friends', function(req,res){
+//         res.json(friends);
+//     })
+
+//     //create new entry
+//     app.post('/api/friends', function (req,res){
+//         //user input object
+//         var userInput=req.body;
+//         console.log("user response: " + JSON.stringify(userInput));
+
+//         var userResponses=userInput.scores;
+        
+//     })
+// }
