51ade795ef07c8237d2b8620f7dd0590f6acebc4
Kate Foust <kate.e.foust@gmail.com>
2020-03-08 17:14:47 -0700

    server connects

---
+++ server.js
@@ -0,0 +1,25 @@
+//dependencies
+var express=require("express");
+var bodyParser=require("body-parser");
+var path=require("path");
+
+var app=express();
+
+//set ports
+var PORT=process.env.PORT || 8080;
+
+//helps express app handle data parsing
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded ({extended:true}));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({type:"application/vnd.api+json"}));
+
+//routes
+// require("./app/routing/apiRoutes")(app);
+// require("./app/routing/htmlRoutes")(app);
+
+//start server
+
+app.listen(PORT, function(){
+    console.log ("app listening on PORT " + PORT);
+})
