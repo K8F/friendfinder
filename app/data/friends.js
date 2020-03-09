2168c06684e3f823239e5dca137a9cb9e85b2809
Kate Foust <kate.e.foust@gmail.com>
2020-03-09 15:41:02 -0700

    fixed bug with image size

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,6 +18,12 @@ var friends=[
     name: "Dorian Gray",
     photo: "https://vignette.wikia.nocookie.net/penny-dreadful/images/3/3e/Penny-dreadful-wikia_dorian_gray_01.jpg/revision/latest?cb=20140403103250",
     scores: [5, 2, 2, 4, 4, 5, 5, 2, 4, 1]
+},
+
+{
+"name": "Jake Barnes",
+"photo": "https://media.vanityfair.com/photos/573364486d2ab6815ef622d0/master/w_2560%2Cc_limit/everybody-behaves-badly-ernest-hemingway.jpg",
+"scores": [2,4,5,5,4,3,2,5,5,4]
 }
 
 ];
