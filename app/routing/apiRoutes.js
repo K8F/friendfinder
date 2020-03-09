07135f43dbbe4c7820c1d49d7a7b9bfa07761ac0
Kate Foust <kate.e.foust@gmail.com>
2020-03-08 19:47:08 -0700

    able to post new user data to api

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,23 @@
 var data= require("../data/friends");
 
-function apiRoutes(app){
-    app.get('/api/friends', function(req,res){
+module.exports=function(app){
+    app.get("/api/friends",function(req,res){
         res.json(data);
     });
 
-    // app.post('/api/friends', function(req,res){
+    app.post("/api/friends", function(req,res){
+       console.log(req.body.scores);
 
-    // })
+       var user=req.body;
+
+    //    for (var i=0; i<user.scores.length; i++){
+    //        user.scores[i]
+    //    };
+
+    data.push(user)
+    
+    })
+
 
 }
 
-module.exports=apiRoutes;
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json({type:"application/vnd.api+json"}));
 
 //routes
 require('./app/routing/apiRoutes.js')(app);
-//require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
 //start server
 
