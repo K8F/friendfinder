7939ba77ba4a1d8c5de48a6cf7c264ea800c2c62
Kate Foust <kate.e.foust@gmail.com>
2020-03-08 21:06:14 -0700

    app almost done; issues with getting the modal to pop up

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,25 +3,25 @@
 var friends=[
 {
     name: "Henry Chinaski",
-    photo: "https://en.wikipedia.org/wiki/Charles_Bukowski#/media/File:Charles_Bukowski_smoking.jpg",
+    photo: "https://vignette.wikia.nocookie.net/thegreatgatsby/images/8/86/Jgatsby.png/revision/latest?cb=20130125042956",
     scores:[3, 2, 2, 1, 1, 5, 4, 5, 3, 5]
 },
 
 {
     name: "Emma",
-    photo: "https://https://en.wikipedia.org/wiki/Emma_(novel)#/media/File:Houghton_Typ_805.98.1770_-_Emma,_p_444.jpg.com/DailyEmmaQuotes/photo",
+    photo: "https://vignette.wikia.nocookie.net/thegreatgatsby/images/8/86/Jgatsby.png/revision/latest?cb=20130125042956",
     scores: [5, 5, 5, 3, 2, 3, 2, 1, 5, 2]
 }, 
 
 {
     name: "Daisy Buchanan",
-    photo: "https://upload.wikimedia.org/wikipedia/en/d/df/Daisy_Buchanan.png",
+    photo: "https://vignette.wikia.nocookie.net/thegreatgatsby/images/8/86/Jgatsby.png/revision/latest?cb=20130125042956",
     scores: [1, 1, 1, 5, 5, 5, 3, 1, 3, 4]
 },
 
 {
     name: "Dorian Gray",
-    photo: "https://en.wikipedia.org/wiki/Dorian_Gray_(character)#/media/File:The_Picture_of_Dorian_Gray.jpg",
+    photo: "https://vignette.wikia.nocookie.net/thegreatgatsby/images/8/86/Jgatsby.png/revision/latest?cb=20130125042956",
     scores: [5, 2, 2, 4, 4, 5, 5, 2, 4, 1]
 }
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,23 +1,48 @@
 var data= require("../data/friends");
 
 module.exports=function(app){
+
+    //returns all users in friends.js
     app.get("/api/friends",function(req,res){
         res.json(data);
     });
 
     app.post("/api/friends", function(req,res){
-       console.log(req.body.scores);
 
        var user=req.body;
 
-    //    for (var i=0; i<user.scores.length; i++){
-    //        user.scores[i]
-    //    };
+    // parseInt for scores
+    for(var i = 0; i < user.scores.length; i++) {
+        user.scores[i] = parseInt(user.scores[i]);
+      }
+  
+    //   // default friend match is the first friend but result will be whoever has the minimum difference in scores
+      var matchIndex = 0;
+      var minimumDifference = 40;
+  
+      // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
+      //  whatever the difference is, add to the total difference
+      for(var i = 0; i < data.length; i++) {
+        var totalDifference = 0;
+        for(var j = 0; j < data[i].scores.length; j++) {
+          var difference = Math.abs(user.scores[j] - data[i].scores[j]);
+          totalDifference += difference;
+        }
+  
+    //     // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+        if(totalDifference < minimumDifference) {
+          matchIndex = i;
+          minimumDifference = totalDifference;
+        }
+      }
+  
 
-    data.push(user)
+    data.push(user);
+
+    res.json(data[matchIndex])
     
-    })
+    });
 
 
-}
+};
 
